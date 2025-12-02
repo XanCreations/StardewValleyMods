b8647683be37c58cb792c09e92a5396dc5b51858
Xan <k2.life.321@gmail.com>
2025-12-02 21:57:58 +1000

    Update ModConfig.cs

--- Trinket Chance Config/ModConfig.cs
+++ Trinket Chance Config/ModConfig.cs
@@ -2,7 +2,28 @@ namespace TrinketChanceConfig
 {
     internal class ModConfig
     {
-        // Multiplier for the vanilla trinket drop chance, expressed as a percentage
+        /// <summary>
+        /// Multiplier for the vanilla trinket drop chance, expressed as a percentage
+        /// 0 = 0% (no drops)
+        /// 100 = 100% (vanilla)
+        /// 200 = 200% (double chance)
+        /// 1000 = 1000% (expanded to such a large number because "DisableDuplicateTrinketDrops" being set to true compounds the chance the spawn a trinket the less trinkets there are to find)
+        /// </summary>
         public int TrinketChancePercentOfBase { get; set; } = 100;
+
+        /// <summary>
+        /// If set to true -> disable the specific vanilla trinket IDs from dropping
+        /// This allows custom trinkets like those made from TrinketTinker to drop normally
+        /// </summary>
+        public bool BlockVanillaTrinkets { get; set; } = false;
+
+        /// <summary>
+        /// If set to true -> any player that has obtained a trinket -> will prevent that trinket dropping again
+        /// If set to false -> trinkets drop like normal
+        /// </summary>
+        public bool DisableDuplicateTrinketDrops { get; set; } = false;
+
+
+
     }
 }
